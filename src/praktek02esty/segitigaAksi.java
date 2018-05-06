e277b9c0aed607f68dc850d6e94a5d0f75093988
adityalesmana <38103892+adityalesmana@users.noreply.github.com>
2018-05-06 06:52:59 -0700

    Membuat class aksi dan method main

---
+++ src/praktek02esty/segitigaAksi.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek02esty;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class segitigaAksi {
+    public static void main(String[] args) {
+}
+}
