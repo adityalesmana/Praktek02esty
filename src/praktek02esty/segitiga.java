cab7d7dbee8e2062975503b9fd41b746d9e1c6ec
adityalesmana <38103892+adityalesmana@users.noreply.github.com>
2018-05-06 06:50:38 -0700

    Membuat attribute dan behavior

--- src/praktek02esty/segitiga.java
+++ src/praktek02esty/segitiga.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek02esty;
 
-/**
- *
- * @author LENOVO
- */
 public class segitiga {
+    double alas;
+    double tinggi;
     
+    void cetakInfo() {
+        System.out.println("==================");
+        System.out.println("alas   :"+alas);
+        System.out.println("tinggi :"+tinggi);
+        System.out.println("==================");
+}
 }
